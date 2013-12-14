4d21cf1d34cdc2c984e1af497289dd841db07d12
Burak Alkan @vmware-ubuntu <balkan@burakalkan.com>
2013-12-14 12:46:16 -0800

    remove admin urls

--- chatproject/urls.py
+++ chatproject/urls.py
@@ -7,6 +7,5 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'chatproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 )
