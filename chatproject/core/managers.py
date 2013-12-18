6d5fa16591ce07c0eafb94fe13c13f8213278af0
Bahattin Çiniç <bahattincinic@gmail.com>
2013-12-18 06:29:05 +0000

    managers.py edited

--- chatproject/core/managers.py
+++ chatproject/core/managers.py
@@ -4,7 +4,7 @@ from django.db import models
 class FilteringManager(models.Manager):
     def __init__(self, **kwargs):
         self.filtering = kwargs
-        super(FilteringManager, self).__init__()
+        return super(FilteringManager, self).__init__()
 
     def get_query_set(self):
         return super(FilteringManager, self).get_queryset()\
