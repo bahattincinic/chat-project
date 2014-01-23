3b84037a400b89ee03cd256a2adb6dbfed13451b
Bahattin Cinic <bahattincinic@gmail.com>
2014-01-23 22:26:06 +0200

    js comment edit

--- chatproject/chat_static/js/apps/apps.js
+++ chatproject/chat_static/js/apps/apps.js
@@ -1,21 +1,22 @@
 'use strict';
 
-// Define all your modules with no dependencies
+// Define all modules with no dependencies
+// authantication application
 angular.module('authApp', []);
+// chat application
 angular.module('chatApp', []);
+// network application
 angular.module('networkApp', []);
 
-// Lastly, define your "main" module and inject all other modules as dependencies
-angular.module('mainApp', [
-    // Angular Apps
-    'ngAnimate',
-    'ngRoute',
-    'ngResource',
-    // Local Apps
-    'authApp',
-    'chatApp',
-    'networkApp'
-]).config(['$httpProvider', function($httpProvider) {
+// Angular Apps
+var ANGULAR_APPS = ['ngAnimate', 'ngRoute', 'ngResource'];
+// Local Apps
+var LOCAL_APPS = ['authApp', 'chatApp', 'networkApp'];
+// All Apps
+var INSTALLED_APPS = ANGULAR_APPS.concat(LOCAL_APPS);
+
+// Define  "main" module and inject all other modules as dependencies
+angular.module('mainApp', INSTALLED_APPS).config(['$httpProvider', function($httpProvider) {
   $httpProvider.defaults.xsrfCookieName = 'csrftoken';
   $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 }]);
