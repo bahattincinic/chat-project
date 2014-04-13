b0a1694313c69ed697e4d82a649e71ed1ed7b197
Burak Alkan <mburakalkan@gmail.com>
2014-04-13 18:04:49 +0300

    use sqlite bydefault

--- chatproject/configs/base.py
+++ chatproject/configs/base.py
@@ -123,7 +123,7 @@ WSGI_APPLICATION = 'chatproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
