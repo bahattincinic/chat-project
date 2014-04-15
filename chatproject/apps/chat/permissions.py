1622ec26ba824338c47ec4c3d45d3bb2bed99e50
Burak Alkan <burak.alkan@markafoni.com>
2014-04-15 11:08:22 +0300

    remove unused code

--- chatproject/apps/chat/permissions.py
+++ chatproject/apps/chat/permissions.py
@@ -7,13 +7,11 @@ from rest_framework.permissions import SAFE_METHODS
 
 class IsPostOrActiveAuthenticated(BasePermission):
     def has_permission(self, request, view):
-        print self.__class__.__name__
         if request.method in SAFE_METHODS:
             user = request.user
             if user and user.is_authenticated() and user.is_active:
                 return True
             return False
-        print 'ret true'
         return True
 
 
@@ -31,7 +29,6 @@ class IsPostOrRequestingUserMatchesUsername(BasePermission):
 
 class IsRequestingUserDiffThanUrl(BasePermission):
     def has_permission(self, request, view):
-        print self.__class__.__name__
         if request.method in SAFE_METHODS:
             return True
         elif request.method in ('POST',) and \
@@ -40,7 +37,6 @@ class IsRequestingUserDiffThanUrl(BasePermission):
             username = view.kwargs.get('username')
             user = User.actives.get_or_raise(username=username, exc=Http404())
             if request.user.id != user.id:
-                print 'ret tr 2 tim'
                 return True
             return False
         return True
--- chatproject/apps/internal/permissions.py
+++ chatproject/apps/internal/permissions.py
@@ -7,7 +7,6 @@ logger = logging.getLogger(__name__)
 
 class LocalPermission(BasePermission):
     def has_permission(self, request, view):
-        print 'permission runnin'
         logger.error('logger error')
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
         if x_forwarded_for:
@@ -16,6 +15,5 @@ class LocalPermission(BasePermission):
             ip = request.META.get('REMOTE_ADDR')
         if not str(ip) in settings.INTERNAL_ALLOWED:
             logger.error("Deny ip: %s from reaching internal api" % str(ip))
-            print 'deny ip %s' % (str(ip))
             return False
         return True
